df2343bb8f68ab376d9d791d3629f340819e96a9
jaeminChung <hijam76@gmail.com>
2016-04-15 17:33:56 +0900

    first commit

---
+++ custom.el
@@ -0,0 +1,33 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(current-language-environment "UTF-8")
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(custom-safe-themes
+   (quote
+    ("b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" default)))
+ '(default-input-method "korean-hangul")
+ '(desktop-save-mode t)
+ '(global-linum-mode t)
+ '(ido-everywhere t)
+ '(ido-mode (quote both) nil (ido))
+ '(make-backup-files nil)
+ '(org-agenda-files (quote ("~/org/")))
+ '(org-src-fontify-natively t)
+ '(recentf-mode t)
+ '(show-paren-mode t)
+ '(tab-width 4)
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#181a26" :foreground "gray80" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "outline" :family "D2Coding")))))
