76003434339acbc6d12229785f8185e0936f1968
hijam <hijam76@gmail.com>
2018-01-22 22:09:02 +0900

    https://github.com/daedreth/UncleDavesEmacs/ 를 참고해서 싹다 바꿈

--- init.el
+++ init.el
@@ -1,32 +1,53 @@
-;;; Begin initialization
-;; Turn off mouse interface early in startup to avoid momentary display
-(when window-system
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (tooltip-mode -1))
-
-(setq inhibit-startup-message t)
-(setq initial-scratch-message "")
-
-;;; Set up package
+;;; This is all kind of necessary
 (require 'package)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;;; Bootstrap use-package
-;; Install use-package if it's not already installed.
-;; use-package is used to configure the rest of the packages.
+;;; Bootstrapping use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-when-compile
-  (require 'use-package))
-(require 'bind-key)
-;(setq use-package-verbose t)
+;;; Making sure theme theme is installed now
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
 
-;;; Load the config
-(require 'org)
+;;; This is the actual config file
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(current-language-environment "UTF-8")
+ '(custom-enabled-themes (quote (spacemacs-dark)))
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(default-input-method "korean-hangul")
+ '(desktop-save-mode t)
+ '(ido-everywhere t)
+ '(ido-mode (quote both) nil (ido))
+ '(make-backup-files nil)
+ '(org-agenda-files (quote ("~/org/")))
+ '(org-src-fontify-natively t)
+ '(package-selected-packages
+   (quote
+    (avy which-key markdown-mode magit use-package slime auto-complete)))
+ '(recentf-mode t)
+ '(show-paren-mode t)
+ '(tab-width 4)
+ '(tool-bar-mode nil)
+ '(tramp-syntax (quote default) nil (tramp)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
